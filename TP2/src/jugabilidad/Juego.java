fba3666724eb21d00b6522dece9fb7bac77b66f0
joaquin <j@j>
2018-06-09 12:00:19 -0300

    aaa

--- TP2/src/jugabilidad/Juego.java
+++ TP2/src/jugabilidad/Juego.java
@@ -1,10 +1,16 @@
 package jugabilidad;
 
+import cartas.Carta;
+
 public class Juego {
 	
 	
 	
 	public void agregarCartasALaMano(int cantidadDeCartas) {
+		
+	}
+	
+	public void mandarCartaAlCementerio(Carta unaCarta) {
 		
 	}
 }
--- TP2/src/jugabilidad/Mazo.java
+++ TP2/src/jugabilidad/Mazo.java
@@ -1,5 +1,15 @@
 package jugabilidad;
 
+import java.util.ArrayList;
+
+import cartas.Monstruo;
+
 public class Mazo {
 
+	private ArrayList<Monstruo> mostruos;
+	private ArrayList<DeCampo> campos;
+	private ArrayList<Magica> magicas;
+	private ArrayList<Trampa> trampas;
+	private ArrayList<Cartas> cartas;
+	
 }
