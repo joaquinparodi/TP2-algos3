38ae6fcbffb9dc3a846f72a933201f713984a5ec
javier <javiferreyra97@hotmail.com>
2018-06-10 03:23:04 -0300

    Jugador y sus tests

--- TP2/src/jugabilidad/Jugador.java
+++ TP2/src/jugabilidad/Jugador.java
@@ -2,6 +2,13 @@ package jugabilidad;
 
 public class Jugador {
 
-	
-	
+    double vida;
+
+	public void Jugador (double vida){
+	    this.vida = vida;
+    }
+
+    public double obtenerVida(){
+	    return this.vida;
+    }
 }
