c3cd5e0d5ace7a0820a1e116f659c18e6422a36c
javier <javiferreyra97@hotmail.com>
2018-06-11 20:40:05 -0300

    sin wanings

--- TP2/src/cartas/AgujeroNegro.java
+++ TP2/src/cartas/AgujeroNegro.java
@@ -1,9 +1,18 @@
 package cartas;
 
+import jugabilidad.Jugador;
+
 public class AgujeroNegro extends Magica{
 	
+	public AgujeroNegro(String auxNombre, Jugador auxJugador) {
+		super(auxNombre, auxJugador);
+	}
+
 	public void aplicarEfecto() {
-		CampoDeJuego campo = campo.obtenerCampo();
+	}
+
+	@Override
+	public void enviarAlCementerio() {		
 	}
 	
 }
