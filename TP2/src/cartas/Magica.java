bb22e285cfb3b6788187c25c3336fb9f69296811
javier <javiferreyra97@hotmail.com>
2018-06-17 23:05:24 -0300

    el efecto es protected de la carta

--- TP2/src/cartas/Magica.java
+++ TP2/src/cartas/Magica.java
@@ -7,8 +7,6 @@ import posiciones.Orientacion;
 
 public class Magica extends Carta{
 
-	private Efecto efecto;
-
 	public Magica(String auxNombre, Jugador auxJugador,Efecto unEfecto) {
 		super(auxNombre, auxJugador);
 		this.efecto = unEfecto;
