b218c2e5f16d168a73be2f159927d3616fb02aae
javier <javiferreyra97@hotmail.com>
2018-06-21 13:09:02 -0300

    conflictos

--- TP2/src/jugabilidad/Jugador.java
+++ TP2/src/jugabilidad/Jugador.java
@@ -9,6 +9,8 @@ import errores.ErrorIntentaAtacarACartaPropia;
 import errores.ErrorSacrificiosInsuficientes;
 import errores.ErrorCartasSacrificadasIncorrectas;
 
+import java.util.Iterator;
+
 import atributos.Sacrificio;
 import atributos.Vida;
 import cartas.Atacable;
