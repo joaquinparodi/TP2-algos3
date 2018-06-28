a79afa7bc6a51576a055e1ab4608acbd9c5fa7a0
joaquin <j@j>
2018-06-27 23:47:36 -0300

    hay ganador arregle el metodo y agregue ahora salta cartel cuando uno gana

--- TP2/src/jugabilidad/Controlador.java
+++ TP2/src/jugabilidad/Controlador.java
@@ -78,7 +78,7 @@ public class Controlador {
 	/*Metodos usador para determinar ganador*/
 	
 	private boolean restanCartasEnMazo() { 
-		return !defensor.poseeCartasEnMazo(); 
+		return defensor.poseeCartasEnMazo(); 
 	}
 	
 	private boolean exodiaEstaCompleto() {
--- TP2/src/jugabilidad/Randomizador.java
+++ TP2/src/jugabilidad/Randomizador.java
@@ -28,6 +28,7 @@ public class Randomizador {
 	
 		
 		for(int i = 0; i < 20; i++) {
+	
 			int numeroAlAzar = (int) (Math.random() * 7) + 1;
 			
 			switch (numeroAlAzar) {
