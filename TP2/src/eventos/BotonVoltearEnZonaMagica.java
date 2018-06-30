fb11265587adc8ba4783ae3e337499d58dc7a3cd
javier2409 <javiferreyra97@hotmail.com>
2018-06-30 19:34:32 -0300

    te avisa cuando alguien usa una carta magica

--- TP2/src/eventos/BotonVoltearEnZonaMagica.java
+++ TP2/src/eventos/BotonVoltearEnZonaMagica.java
@@ -4,6 +4,10 @@ import Vista.VentanaDeJuego;
 import cartas.Carta;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.stage.Window;
 
 public class BotonVoltearEnZonaMagica implements EventHandler<ActionEvent> {
 
@@ -18,6 +22,18 @@ public class BotonVoltearEnZonaMagica implements EventHandler<ActionEvent> {
 	public void handle(ActionEvent event) {		
 		carta.voltear();
 		ventana.actualizarCampoDeJuego();
+		Alert alerta = new Alert(AlertType.INFORMATION);
+		alerta.initOwner(ventana.getStage());
+		alerta.setTitle("Resultado");
+		alerta.setHeaderText("");
+		String texto;
+		
+		texto = carta.obtenerDuenio().obtenerNombre()+" ha usado la carta magica "+carta.obtenerNombre();
+		
+		alerta.setContentText(texto);
+		
+		alerta.showAndWait();
+
 	}
 	
 	 
